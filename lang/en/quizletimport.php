b52d95bd6fd5ff74d0b1837cb22e1d2d7d3ce0c5
Justin Hunt <poodllsupport@gmail.com>
2014-01-28 23:29:47 +0900

    initial commit of quizlet import

---
+++ lang/en/quizletimport.php
@@ -0,0 +1,46 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for quizletimport
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_quizletimport
+ * @copyright  2011 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'quizletimport';
+$string['modulenameplural'] = 'quizletimports';
+$string['modulename_help'] = 'Use the quizletimport module for... | The quizletimport module allows...';
+$string['quizletimportfieldset'] = 'Custom example fieldset';
+$string['quizletimportname'] = 'quizletimport name';
+$string['quizletimportname_help'] = 'This is the content of the help tooltip associated with the quizletimportname field. Markdown syntax is supported.';
+$string['quizletimport'] = 'quizletimport';
+$string['pluginadministration'] = 'quizletimport administration';
+$string['pluginname'] = 'quizletimport';
+
+$string['displayoptions'] = 'qi display options';
+$string['configdisplayoptions'] = 'qi cfg display options';
+$string['printheading'] = 'qi printheading';
+$string['printheadingexplain'] = 'qi printheading expl';
+
